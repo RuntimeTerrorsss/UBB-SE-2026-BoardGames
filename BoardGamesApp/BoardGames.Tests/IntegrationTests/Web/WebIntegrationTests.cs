757f6a0281ff38e85aa7d54c65646a6dbf56e6f9
EmrichDenisa <denisa.emrich@stud.ubbcluj.ro>
2026-05-29 13:15:31 +0300

    a

---
+++ BoardGamesApp/BoardGames.Tests/IntegrationTests/Web/WebIntegrationTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BoardGames.Tests.IntegrationTests.Web
+{
+    [TestFixture]
+    [Category("Integration")]
+    public sealed class WebIntegrationTests
+    {
+        private WebAppFactory factory = null!;
+        private HttpClient client = null!;
+
+        [SetUp]
+        public void SetUp()
+        {
+            factory = new WebAppFactory();
+            client = factory.CreateClient();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            client.Dispose();
+            factory.Dispose();
+        }
+
+        [Test]
+        public async Task Home_Index_ReturnsHtml()
+        {
+            var response = await client.GetAsync("/");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.That(content, Does.Contain("<html").IgnoreCase);
+        }
+    }
+}
